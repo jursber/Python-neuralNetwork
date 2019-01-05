82f8fa927d84f25e5dab7dae4396975da0f0dcd4
jursber <jursber@163.com>
2019-01-06 00:15:53 +0800

    优化文件读取

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import neuralNetwork as nt
 import numpy as np
 import time
 import os
+from multiprocessing import Pool
 import scipy.ndimage
 import weChatReminder as wcr
 import itchat
@@ -15,148 +16,134 @@ import itchat
 
 #将原数据图像旋转±10度，生成新的训练集，丰富训练数据
 def rotate_data():
-    start_time=time.time()
+    start_time = time.time()
     #旋转函数
     plus10=lambda x:scipy.ndimage.interpolation.rotate(x.reshape(28,28),10,cval=.01,reshape=False).reshape(784)
     minus10=lambda x:scipy.ndimage.interpolation.rotate(x.reshape(28,28),-10,cval=.01,reshape=False).reshape(784)
-    
-    with open('mnist_dataset/mnist_train.csv','r') as training_data_file:
-        training_data_list=training_data_file.readlines()
-    
+
+    with open('mnist_dataset/mnist_train.csv', 'r') as training_data_file:
+        training_data_list = training_data_file.readlines()
+
     #旋转后的目标值和训练值
-    rotate_target_data=[]
-    rotate_train_data=[]
+    rotate_target_data = []
+    rotate_train_data = []
     for record in training_data_list:
         rotate_target_data.append(np.asfarray(record.split(',')[0]))
-        rotate_train_data.append(np.asfarray(record.split(',')[1:])) 
-    train_data_plus10=np.array(list(map(plus10,rotate_train_data)))
-    train_data_minus10=np.array(list(map(minus10,rotate_train_data)))
-    rotate_target_data=np.array(rotate_target_data)
-    
+        rotate_train_data.append(np.asfarray(record.split(',')[1:]))
+    train_data_plus10 = np.array(list(map(plus10, rotate_train_data)))
+    train_data_minus10 = np.array(list(map(minus10, rotate_train_data)))
+    rotate_target_data = np.array(rotate_target_data)
+
     #合并数据
-    csv_plus10=np.c_[rotate_target_data,train_data_plus10]
-    csv_minus10=np.c_[rotate_target_data,train_data_minus10]
-    
+    csv_plus10 = np.c_[rotate_target_data, train_data_plus10]
+    csv_minus10 = np.c_[rotate_target_data, train_data_minus10]
+
     #存入文件
-    np.savetxt('mnist_dataset/new_train_plus10.csv',csv_plus10,delimiter=',')
-    np.savetxt('mnist_dataset/new_train_minus10.csv',csv_minus10,delimiter=',')
-    
-    wcr.send_wechat_message('训练集生成完毕，总计用时:{}s'.format(round(time.time()-start_time,0)))
-    
+    np.savetxt('mnist_dataset/new_train_plus10.csv', csv_plus10, delimiter=',')
+    np.savetxt(
+        'mnist_dataset/new_train_minus10.csv', csv_minus10, delimiter=',')
+
+    wcr.send_wechat_message('训练集生成完毕，总计用时:{}s'.format(
+        round(time.time() - start_time, 0)))
+
+#读取训练文件
+def read_csv(file):
+    with open(file, 'r') as f:
+        data = f.readlines()
+    return data
+
 #训练神经网络
-def training_neuralNework():
+def training_neuralNework(training_data_list, epochs=1):
     #记录程序开始时间
     start_time = time.time()
+
     #删除原训练数据
     try:
         os.remove("weight_data/who.csv")
         os.remove("weight_data/wih.csv")
-    except:
+    except IOError:
         pass
-    
-    #全部训练集
-    training_data_path=['mnist_dataset/mnist_train.csv',
-                        'mnist_dataset/new_train_minus10.csv',
-                        'mnist_dataset/new_train_plus10.csv']
-    training_data_list=[] #全部训练集存入数组
-    #读取文件
-    for path in training_data_path:
-        with open(path,'r') as training_data_file:
-            training_data_list.append(np.array(training_data_file.readlines()))
-        #training_data_list=np.hstack((training_data_list1,training_data_list2))
-    print('文件读取完成，读取用时：{}s'.format(int(time.time()-start_time)))
-    
-    train_count=0
-    epochs=8 #训练集应用次数，世代
-    #开始训练
-    for e in range(epochs):
-        print('第{}次循环'.format(e+1))
-        data_count=1
-        for item in training_data_list:
-            print('训练集：NO.',data_count)
-            for record in item:
-                all_values=record.split(',')
-                #归一化输入，0~255 int变成0.01~1 float
-                inputs=np.asfarray(all_values[1:])/255*.99+.01
-                #归一化target，0~9变成数组
-                targets=np.zeros(output_nodes)+0.01
-                targets[int(float(all_values[0]))]=0.99
-                #开始训练
-                n.train(inputs,targets) 
-                train_count+=1
-                #print(n.wih[:2,::560])
-                #print(n.who[:2,::10])
-            data_count+=1
-        wcr.send_wechat_message('第{}次循环（训练）完成！，总计用时{}s'.format(e+1,round(time.time()-start_time,0)))
+
+    train_count = 0
+    for e in range(epochs):  #开始训练
+        print('第{}次循环'.format(e + 1))
+        data_count = 1
+        for record in training_data_list:
+            all_values = record.split(',')
+            #归一化输入，0~255 int变成0.01~1 float
+            inputs = np.asfarray(all_values[1:]) / 255 * .99 + .01
+            #归一化target，0~9变成数组
+            targets = np.zeros(output_nodes) + 0.01
+            targets[int(float(all_values[0]))] = 0.99
+            #开始训练
+            n.train(inputs, targets)
+            train_count += 1
+            #print(n.wih[:2,::560])
+            #print(n.who[:2,::10])
+        data_count += 1
+        wcr.send_wechat_message('第{}次循环（训练）完成！，总计用时{}s'
+            .format(e + 1, round(time.time() - start_time, 0)))
 
     #训练数据存入文件
-    np.savetxt('weight_data/wih.csv',n.wih,delimiter=',')
-    np.savetxt('weight_data/who.csv',n.who,delimiter=',')
-    #记录程序运行时长   
-    m, s = divmod(int(time.time()-start_time), 60)
+    np.savetxt('weight_data/wih.csv', n.wih, delimiter=',')
+    np.savetxt('weight_data/who.csv', n.who, delimiter=',')
+    #记录程序运行时长
+    m, s = divmod(int(time.time() - start_time), 60)
     h, m = divmod(m, 60)
     wcr.send_wechat_message('训练完成，训练数据{}行，循环{}次，总计用时{:02d}:{:02d}:{:02d}'
-          .format(train_count/epochs,train_count,h,m,s))
+                            .format(train_count / epochs, train_count, h, m, s))
 
-#测试神经网络   
-def test_neuralNework():     
-    with open('mnist_dataset/mnist_test.csv','r') as test_data_file:
-        test_data_list=test_data_file.readlines()
-    
+#测试神经网络
+def test_neuralNework():
+    with open('mnist_dataset/mnist_test.csv', 'r') as test_data_file:
+        test_data_list = test_data_file.readlines()
     #对识别效果计分
-    scorecard=[]
+    scorecard = []
     for record in test_data_list:
-        all_values=record.split(',')
-        correct_label=int(all_values[0])
+        all_values = record.split(',')
+        correct_label = int(all_values[0])
         #处理输入
-        inputs=np.asfarray(all_values[1:])/255*.99+.01
+        inputs = np.asfarray(all_values[1:]) / 255 * .99 + .01
         #通过神经网络输出
-        outputs=n.query(inputs)
+        outputs = n.query(inputs)
         #记录正确率结果的数量
-        label=np.argmax(outputs)
-        if label==correct_label:
+        label = np.argmax(outputs)
+        if label == correct_label:
             scorecard.append(1)
         else:
             scorecard.append(0)
-    wcr.send_wechat_message('{}个输出结果中，正确的个数为{}，正确率：{:.2%}'
-          .format(len(scorecard),scorecard.count(1),scorecard.count(1)/len(scorecard)))
-    
-    
-if __name__=='__main__':
+    wcr.send_wechat_message('{}个输出结果中，正确的个数为{}，正确率：{:.2%}'.format(
+        len(scorecard), scorecard.count(1),
+        scorecard.count(1) / len(scorecard)))
+
+if __name__ == '__main__':
     wcr.send_wechat_message('Python神经网络程序开始运行！')
-    #设置每层节点
-    input_nodes,hidden_nodes,output_nodes=784,150,10
-    #设置学习率
-    learning_rate=.1
+    #设置每层节点 & 学习率
+    input_nodes, hidden_nodes, output_nodes = 784, 150, 10
+    learning_rate = .15
     #创建神经网络对象
-    n=nt.neuralNetwork(input_nodes,hidden_nodes,output_nodes,learning_rate)
-    #扩充训练集
-    rotate_data()
-    #执行程序
-    training_neuralNework()
-    test_neuralNework()
-    
+    n = nt.neuralNetwork(input_nodes, hidden_nodes, output_nodes,
+                         learning_rate)
+
+    rotate_data() #扩充训练集
+
+    #多线程读取文件
+    read_start_time = time.time()
+    training_data_path = [
+        'mnist_dataset/mnist_train.csv', 'mnist_dataset/new_train_minus10.csv',
+        'mnist_dataset/new_train_plus10.csv'
+    ]  # 全部训练集
+    training_data_list = []  #全部训练集存入数组
+    pool = Pool(processes=3)
+    for f in training_data_path:
+        training_data_list.extend(pool.apply_async(read_csv, (f, )).get())
+    pool.close()
+    pool.join()
+    print('文件读取完成，读取用时：{}s'.format(int(time.time() - read_start_time)))
+
+    #开始训练
+    training_neuralNework(training_data_list,epochs=5)  
+    #测试训练效果
+    test_neuralNework()  #测试训练效果
+
 #    itchat.logout()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- neuralNetwork.py
+++ neuralNetwork.py
@@ -4,76 +4,91 @@
 初始化、训练、查询
 """
 import numpy as np
-from scipy.special import expit , logit
+from scipy.special import expit, logit
 import os
+
 
 class neuralNetwork:
     #初始化神经网络
-    def __init__(self,inputnodes,hiddennodes,outputnodes,learningrate):
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
         #神经网络各层初始参数
-        self.inodes=inputnodes
-        self.hnodes=hiddennodes
-        self.onodes=outputnodes
-        
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+
         #学习率
-        self.lr=learningrate
-        
+        self.lr = learningrate
+
         if self.init_check():
             #若存在权重文件，则使用
-            self.wih=np.loadtxt(open("weight_data/wih.csv","r"), delimiter=",", skiprows=0)
-            self.who=np.loadtxt(open("weight_data/who.csv","r"), delimiter=",", skiprows=0)
-        else:                       
+            self.wih = np.loadtxt(
+                open("weight_data/wih.csv", "r"), delimiter=",", skiprows=0)
+            self.who = np.loadtxt(
+                open("weight_data/who.csv", "r"), delimiter=",", skiprows=0)
+        else:
             #若不存在权重文件，初始化权重网络,mean=0,std=1/((连接数)^(1/2))
-            self.wih=np.random.normal(0.0,pow(self.hnodes,-.5),(self.hnodes,self.inodes))
-            self.who=np.random.normal(0.0,pow(self.onodes,-.5),(self.onodes,self.hnodes))   
-        
+            self.wih = np.random.normal(0.0, pow(self.hnodes, -.5),
+                                        (self.hnodes, self.inodes))
+            self.who = np.random.normal(0.0, pow(self.onodes, -.5),
+                                        (self.onodes, self.hnodes))
+
         #利用现有库创建Sigmoid函数
-        self.activation_function=expit
-           
+        self.activation_function = expit
+
     #验证是否存在已训练好的权重文件，文件是否有效
     def init_check(self):
-        if os.path.exists('weight_data/wih.csv') and os.path.exists('weight_data/who.csv'):
-            if (np.loadtxt(open("weight_data/wih.csv","r"), delimiter=",", skiprows=0).shape==(self.hnodes,self.inodes) and 
-                np.loadtxt(open("weight_data/who.csv","r"), delimiter=",", skiprows=0).shape==(self.onodes,self.hnodes)):
+        if os.path.exists('weight_data/wih.csv') and os.path.exists(
+                'weight_data/who.csv'):
+            if (np.loadtxt(
+                    open("weight_data/wih.csv", "r"), delimiter=",",
+                    skiprows=0).shape == (self.hnodes, self.inodes)
+                    and np.loadtxt(
+                        open("weight_data/who.csv", "r"),
+                        delimiter=",",
+                        skiprows=0).shape == (self.onodes, self.hnodes)):
                 return True
         return False
-        
+
     #训练神经网络
-    def train(self,inputs_list,targets_list):            
-        inputs=np.array(inputs_list,ndmin=2).T
-        targets=np.array(targets_list,ndmin=2).T
-        
+    def train(self, inputs_list, targets_list):
+        inputs = np.array(inputs_list, ndmin=2).T
+        targets = np.array(targets_list, ndmin=2).T
+
         #输出结果计算
-        hidden_inputs=np.dot(self.wih,inputs)
-        hidden_outputs=self.activation_function(hidden_inputs)
-        final_inputs=np.dot(self.who,hidden_outputs)
-        final_outputs=self.activation_function(final_inputs)
-        
+        hidden_inputs = np.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+        final_inputs = np.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+
         #反向传播误差
-        output_errors=targets-final_outputs
-        hidden_errors=np.dot(self.who.T,output_errors) #舍弃归一化因子后的近似误差
-        
+        output_errors = targets - final_outputs
+        hidden_errors = np.dot(self.who.T, output_errors)  #舍弃归一化因子后的近似误差
+
         #更新权重
-        self.who+=self.lr*np.dot((output_errors*final_outputs*(1-final_outputs)),np.transpose(hidden_outputs))
-        self.wih+=self.lr*np.dot((hidden_errors*hidden_outputs*(1-hidden_outputs)),np.transpose(inputs))
-        
+        self.who += self.lr * np.dot(
+            (output_errors * final_outputs * (1 - final_outputs)),
+            np.transpose(hidden_outputs))
+        self.wih += self.lr * np.dot(
+            (hidden_errors * hidden_outputs * (1 - hidden_outputs)),
+            np.transpose(inputs))
+
     #神经网络结果计算
-    def query(self,inputs_list):
-        inputs=np.array(inputs_list,ndmin=2).T
+    def query(self, inputs_list):
+        inputs = np.array(inputs_list, ndmin=2).T
         #隐藏层计算
-        hidden_inputs=np.dot(self.wih,inputs)
-        hidden_outputs=self.activation_function(hidden_inputs)
-        
+        hidden_inputs = np.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+
         #输出层计算
-        final_inputs=np.dot(self.who,hidden_outputs)
-        final_outputs=self.activation_function(final_inputs)
-        
+        final_inputs = np.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+
         return final_outputs
-    
+
     #逆向查询，通过输出数组生成可解释的图像数组（并非原图）
-    def back_query(self,target_list):
+    def back_query(self, target_list):
         #Sigmoid的反函数
-        self.inverse_activation_function=logit        
+        self.inverse_activation_function = logit
         #输出层
         final_outputs = np.array(target_list, ndmin=2).T
         final_inputs = self.inverse_activation_function(final_outputs)
@@ -84,9 +99,9 @@ class neuralNetwork:
         hidden_outputs /= np.max(hidden_outputs)
         hidden_outputs *= 0.98
         hidden_outputs += 0.01
-        
-        hidden_inputs = self.inverse_activation_function(hidden_outputs)   
-        
+
+        hidden_inputs = self.inverse_activation_function(hidden_outputs)
+
         #输入层
         inputs = np.dot(self.wih.T, hidden_inputs)
 
@@ -95,15 +110,3 @@ class neuralNetwork:
         inputs *= 0.98
         inputs += 0.01
         return inputs
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    
-    
